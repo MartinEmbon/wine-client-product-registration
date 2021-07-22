a530e640aa35a250027383a10f03419d181e8e2d
Martincho <martinembon@hotmail.com>
2021-07-22 08:58:10 -0300

    push multiple items to LS

--- assets/javascript/clientels.js
+++ assets/javascript/clientels.js
@@ -1,28 +1,37 @@
 const btnSend = document.getElementById("submit")
 const btnRead = document.getElementById("readProduct")
 
+const getLocalStorage = () => JSON.parse(localStorage.getItem('client')) ?? []
 
-const getLocalStorage = () => JSON.parse(localStorage.getItem('client')) ?? []    
 
-btnSend.onclick=function() {
-    const client = {
+const saveClient = ()=>{
+    let client = {
         nome: document.getElementById("nome").value,
         cpf: document.getElementById("cpf").value,
         nacionalidade: document.getElementById("nacionalidade").value,        
         email: document.getElementById("email").value,
         tel: document.getElementById("tel").value,
         cep: document.getElementById("cep").value,   
-        endereco: document.getElementById("endereco").value      ,
+        endereco: document.getElementById("endereco").value,
         num: document.getElementById("num").value,
         bairro: document.getElementById("bairro").value,
         cidade: document.getElementById("cidade").value,
         uf: document.getElementById("uf").value
-    }
-    
-    localStorage.setItem("client", JSON.stringify(client))
+    }      
+    createClient(client)
 }
 
-btnRead.addEventListener("click",()=>{
+const createClient = (client) => {
+    const dbClient = getLocalStorage()
+    dbClient.push(client)
+    localStorage.setItem("client",JSON.stringify(dbClient))
+}
+
+
+const readClient = () => getLocalStorage()
+
+btnSend.addEventListener("click", saveClient)
+
+btnRead.addEventListener("click", ()=>{
     console.log(getLocalStorage())
 })
-
