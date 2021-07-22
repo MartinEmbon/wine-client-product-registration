d6ada948ac577c77770e72f422fcde8e5d35004c
MartinEmbon <77815749+MartinEmbon@users.noreply.github.com>
2021-07-22 09:00:21 -0300

    Add files via upload

--- assets/javascript/produtols.js
+++ assets/javascript/produtols.js
@@ -1,22 +1,31 @@
 const btnSend = document.getElementById("submit")
 const btnRead = document.getElementById("readProduct")
 
-const getLocalStorage = () => JSON.parse(localStorage.getItem('product')) ?? []
-
-btnSend.onclick=function() {
-    const product = {
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_product')) ?? []
+//const setLocalStorage = (dbProduct) => localStorage.setItem("product", JSON.stringify(dbProduct))
+  
+const saveProduct = ()=>{
+    let product = {
         categoria: document.getElementById("categoria").value,
         rotulo: document.getElementById("rotulo").value,
         descricao: document.getElementById("descricao").value,
         precioCompra: document.getElementById("compra").value,
         precioVenda: document.getElementById("venda").value      
-    }
-    
-    localStorage.setItem("product", JSON.stringify(product))
+    }      
+    createProduct(product)
 }
 
-btnRead.addEventListener("click",()=>{
+const createProduct = (product) => {
+    const dbProduct = getLocalStorage()
+    dbProduct.push(product)
+    localStorage.setItem("db_product",JSON.stringify(dbProduct))
+}
+
+const readProduct = () => getLocalStorage()
+
+btnSend.addEventListener("click", saveProduct)
+
+btnRead.addEventListener("click", ()=>{
     console.log(getLocalStorage())
 })
-
 
